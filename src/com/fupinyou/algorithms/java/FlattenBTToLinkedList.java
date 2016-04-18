1e1dc42f5d714b6464efc3cb3ec5955d4084049b
fupinyou <pinyoufu@outlook.com>
2016-04-18 20:23:56 +0800

    add 1 solution

---
+++ src/com/fupinyou/algorithms/java/FlattenBTToLinkedList.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+/**
+ * Created by fupinyou on 2016/4/18.
+ */
+public class FlattenBTToLinkedList {
+    public class TreeNode {
+             public int val;
+             public TreeNode left, right;
+             public TreeNode(int val) {
+                     this.val = val;
+                     this.left = this.right = null;
+                 }
+         }
+    public void flatten(TreeNode root) {
+        // write your code here
+        if(root==null) return;
+        ArrayList<Integer> rst=preorderTraversal(root);
+        TreeNode tn=root;
+        for(int i=1;i<rst.size();i++){
+            tn.left=null;
+            tn.right=new TreeNode(rst.get(i));
+            tn=tn.right;
+        }
+    }
+
+    public ArrayList<Integer> preorderTraversal(TreeNode root){
+        ArrayList<Integer> arra=new ArrayList<>();
+        if(root==null){
+            return arra;
+        }
+        arra.add(root.val);
+        if (root.left!=null) {
+            ArrayList<Integer> ali=preorderTraversal(root.left);
+            if(ali!=null) {
+                arra.addAll(ali);
+            }
+        }
+
+        if(root.right!=null){
+            ArrayList<Integer> ali=preorderTraversal(root.right);
+            if(ali!=null) {
+                arra.addAll(ali);
+            }
+        }
+        return arra;
+    }
+}
