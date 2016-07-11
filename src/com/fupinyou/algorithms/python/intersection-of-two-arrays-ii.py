5be80b9db7f8579fd22ef9c7d175b7f8d0f8c03a
fupinyou <pinyoufu@outlook.com>
2016-07-11 16:05:30 +0800

    add a python file

---
+++ src/com/fupinyou/algorithms/python/intersection-of-two-arrays-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    # @param {int[]} nums1 an integer array
+    # @param {int[]} nums2 an integer array
+    # @return {int[]} an integer array
+    def intersection(self, nums1, nums2):
+        # Write your code here
+        if nums1 == nums2:
+            return nums1
+        nums3 = []
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        size = len(nums1)
+        for i in xrange(0, size, 1):
+            if nums1[i] in nums3:
+                continue
+            if nums1[i] in nums2:
+                number = min(nums1.count(nums1[i]), nums2.count(nums1[i]))
+                for j in xrange(number):
+                    nums3.append(nums1[i])
+        return nums3
+
+
+A = [1, 2, 2, 1]
+B = [2, 2]
+solution = Solution()
+C = solution.intersection(A, B)
+print C
